cc09a14912c7850a7b9eadb9b5918f3788d735b7
Akouvi Kouedjin <kouedjinl@gmail.com>
2021-11-17 12:56:50 -0500

    navigation tabs addition

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// https://www.youtube.com/watch?v=7CqJlxBYj-M&ab_channel=freeCodeCamp.org
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercises from './components/edit-exercises.component';
+import CreateExercises from './components/create-exercises.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Navbar />
+        <br />
+        <Routes>
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercises} />
+          <Route path="/create" component={CreateExercises} />
+          <Route path="/user" component={CreateUser} />
+        </Routes>
+      </Router>
     </div>
   );
 }
